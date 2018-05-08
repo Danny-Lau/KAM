249d9bfaaa992d85693f95bd12f4c2d66f79c7f1
Danny-Lau <31654961+Danny-Lau@users.noreply.github.com>
2018-05-08 21:13:50 +0200

    Layout til myProducts

--- js/myProducts.js
+++ js/myProducts.js
@@ -12,21 +12,28 @@ $(document).ready(() => {
         //For every product the specific information are shown
         product.forEach(product => {
           const sellerProductsHTML = `
-          <div class= "myProductBox">
-          
-            <dt>Description: </dt>
-            <dd>${product.productDescription}</dd>
-            <dt>Stock: </dt>
-            <dd>${product.stock}</dd>
-            <dt>Variant: </dt>
-            <dd>${product.variant}</dd>
-            <dt>Gender: </dt>
-            <dt>${product.gender}</dt>
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="col-lg-8">
+                      <dl>
+                      <div class="infoBox">
+                        <a><img class="productImg product-btn"  src="${product.url}"/></a>
+                       <div> 
+                      </dl>
+                    </div>
+                    <dt>Description: ${product.productDescription}</dt>
+                        <dt>Stock:  ${product.stock} </dt>    
+                        <dt>Size: ${product.variant}</dt>
+                        <dt>Gender: ${product.gender} </dt>
+                        <br>
+                        <br>
+                    <button class="login-btn product-btn" data-product-id="${product.productID}">Edit product</button>
+                    <button class="login-btn deleteproduct-btn" data-delete-product-id="${product.productID}">Delete product</button>
+                </div>
+       
             
-            <button class="login-btn product-btn" data-product-id="${product.productID}">Edit product</button>
-            <button class="login-btn deleteproduct-btn" data-delete-product-id="${product.productID}">Delete product</button>
-           
-        </div>`;
+        `;
 
 
           productsList.append(sellerProductsHTML);
